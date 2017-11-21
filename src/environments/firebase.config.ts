b52604428f82bd8746586aa2c238b8f5292d7240
Junior Rojas <yunior0000@gmail.com>
2017-11-20 19:30:31 -0500

    Update firebase.config.ts

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
 
-    apiKey: 'AIzaSyCL_hHiu0jk7T0bnZMpahKkgEEjZsfr7Ak',
-    authDomain: 'servicioya-d43c7.firebaseapp.com',
-    databaseURL: 'https://servicioya-d43c7.firebaseio.com',
-    projectId: 'servicioya-d43c7',
-    storageBucket: 'servicioya-d43c7.appspot.com',
-    messagingSenderId: '978617072299'
+     apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
 
 };
